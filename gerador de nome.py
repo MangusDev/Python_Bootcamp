0d0324913bc5d54aeaa22e7ebda746fb6f6c8acb
Mangus Dev <dev.mangus@gmail.com>
2026-02-12 18:10:21 -0300

    Removendo arquivos!

--- gerador de nome.py
+++
@@ -1,4 +0,0 @@
-print("Seja bem vindo ao geradaor de nome de banda!")
-city = input("qual e o nome da cidade natal?")
-pet  = input("qual nome do seu animal?")
-print ("O nome da sua banda poderia ser: " + city + " " + pet)
